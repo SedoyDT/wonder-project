9e9c900831646b47ea8b79be42041bfe0fa1c098
SedoyDT <mazzzfaka@gmail.com>
2019-11-16 12:58:26 +0300

    переадресация

--- SB/metanit/4Express/5маршрутизация/app.js
+++ SB/metanit/4Express/5маршрутизация/app.js
@@ -1,21 +1,11 @@
 const express = require("express");
 const app = express();
  
-// обработка запроса по адресу /about
-app.get("/about", function(request, response){
-     
-    response.send("<h1>О сайте</h1>");
+app.use("/home/bar",function (request, response) {
+  response.redirect("/about")
+});
+app.use("/about", function (request, response) {
+  response.send("<h1>About</h1>");
 });
  
-// обработка запроса по адресу /contact
-app.use("/contact", function(request, response){
-     
-    response.send("<h1>Контакты</h1>");
-});
- 
-// обработка запроса к корню веб-сайта
-app.get("/", function(request, response){
-     
-    response.send("<h1>Главная страница</h1>");
-});
 app.listen(3000);
